6fdcd610fd5ece8c10866e2b42e231c935f4386c
thilina_dilshan <thilina.dilshan@arimaclanka.com>
2021-11-09 23:37:57 +0530

    places added to arrays

---
+++ src/types.ts
@@ -0,0 +1,17 @@
+type Place = {
+  name: string;
+  latitude: number;
+  longitude: number;
+  status: string;
+  image_url: string;
+};
+
+const places: Place[] = [];
+
+places.push({
+  name: "Sri Lanka",
+  latitude: 94.0,
+  longitude: 95.67,
+  status: "Beautiful",
+  image_url: "https:srilanka.com",
+});
